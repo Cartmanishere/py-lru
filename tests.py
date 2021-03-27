49c7480112560bb76ead60384c724a0df1ac9718
Cartmanishere <apbetahouse45@gmail.com>
2021-03-27 10:16:14 +0530

    Add unittests for lru cache

--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from py_lru import DoublyLinkedList
+from py_lru import LRUCache
+
 
 class DoublyLinkedListTest(TestCase):
     def setUp(self) -> None:
@@ -39,3 +41,28 @@ class DoublyLinkedListTest(TestCase):
             self.dll.insert_left(i)
         self.assertEqual(0, self.dll.pop_right().val, "Should return rightmost element in the list")
 
+
+class LRUCacheTest(TestCase):
+
+    def test_cache(self):
+        cache = LRUCache(1)
+        cache['foo'] = 'bar'
+        self.assertEqual(cache['foo'], 'bar', "Cache should return the set value")
+
+    def test_max_slots(self):
+        cache = LRUCache(1)
+        cache['foo'] = 'bar'
+        cache['key'] = 'value'
+        self.assertEqual(len(cache), 1, "Cache should contain only 1 item")
+        self.assertEqual(cache['key'], 'value', "Cache should contain recently added value")
+        self.assertIsNone(cache['foo'], "Cache should not contain older value")
+
+    def test_cache_order(self):
+        cache = LRUCache(2)
+        cache['foo'] = 'bar'
+        cache['key'] = 'value'
+        self.assertListEqual(['bar', 'value'], [i for i in cache])
+        _ = cache['foo']
+        self.assertListEqual(['value', 'bar'], [i for i in cache])
+        _ = cache['key']
+        self.assertListEqual(['bar', 'value'], [i for i in cache])
